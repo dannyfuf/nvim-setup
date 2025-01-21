1229a0c605585e35a7952a8dc09cc4edd18cc8b0
Danny Fuentes <dannyfuf@gmail.com>
2025-01-21 03:05:13 -0300

    update nvim config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,25 +4,27 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 function insertFullPath()
-  vim.fn.setreg('+', vim.fn.expand('%:p')) -- write to clippoard
+	vim.fn.setreg("+", vim.fn.expand("%:p")) -- write to clippoard
 end
-vim.keymap.set('n', '<leader>yp', insertFullPath, { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>yp", insertFullPath, { noremap = true, silent = true })
 
--- function runRspec()
---   local filepath = vim.fn.expand('%:p')  -- using %:p for absolute path
---   local cmd = 'doppler run -- bundle exec rspec --color ' .. vim.fn.shellescape(filepath)
+-- supermanven
+-- vim.keymap.set("n", "<S-l>", ":bnext<CR>")
+vim.keymap.set("n", "<S-h>", ":bprevious<CR>")
 
---   vim.cmd('split')
---   vim.cmd('terminal ' .. cmd)
--- end
--- vim.keymap.set('n', '<leader>tf', runRspec, { noremap = true, silent = true })
-
+vim.keymap.set("n", "<leader>pv", ":Ex<CR>")
 
 -- neotest
 local opts = { noremap = true, silent = true }
-vim.keymap.set('n', '<leader>l', ':lua require("neotest").run.run()<CR>', opts)
-vim.keymap.set('n', '<leader>ls', ':lua require("neotest").run.stop()<CR>', opts)
-vim.keymap.set('n', '<leader>lk', ':lua require("neotest").run.run(vim.fn.expand("%"))<CR>', opts)
-vim.keymap.set('n', '<leader>lo', ':lua require("neotest").output.open()<CR>', opts)
-vim.keymap.set('n', '<leader>loo', ':lua require("neotest").output.open({ enter = true })<CR>', opts)
-vim.keymap.set('n', '<leader>ls', ':lua require("neotest").summary.toggle()<CR>', opts)
+vim.keymap.set("n", "<leader>l", ':lua require("neotest").run.run()<CR>', opts)
+vim.keymap.set("n", "<leader>ls", ':lua require("neotest").run.stop()<CR>', opts)
+vim.keymap.set("n", "<leader>lk", ':lua require("neotest").run.run(vim.fn.expand("%"))<CR>', opts)
+vim.keymap.set("n", "<leader>lo", ':lua require("neotest").output.open()<CR>', opts)
+vim.keymap.set("n", "<leader>loo", ':lua require("neotest").output.open({ enter = true })<CR>', opts)
+vim.keymap.set("n", "<leader>ls", ':lua require("neotest").summary.toggle()<CR>', opts)
+
+-- remove highlighted words
+vim.keymap.set("n", "<leader>//", ":nohls<CR>")
+
+-- oil show parent
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
