ea35f555c6ed9213cb55e22e252f67dc2d1593c7
Danny Fuentes <dannyfuf@gmail.com>
2025-05-05 15:39:59 -0400

    updated

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,63 +1,64 @@
 return {
-	{
-		"yetone/avante.nvim",
-		event = "VeryLazy",
-		version = false, -- Never set this value to "*"! Never!
-		keys = {
-			{ "<leader>ad", "<cmd>AvanteClear<cr>", desc = "Clear Avante context" },
-		},
-		opts = {
-			-- add any opts here
-			provider = "claude",
-			anthropic = {
-				model = "claude-3-7-sonnet-20240307", -- Claude 3.7 Sonnet model
-				timeout = 60000, -- Increased timeout to 60 seconds for complex tasks
-				temperature = 0.1, -- Slight randomness for more creative responses while maintaining reliability
-				max_completion_tokens = 12000, -- Increased token limit for more comprehensive responses
-				top_p = 0.95, -- Adding top_p for better response quality
-				top_k = 50, -- Adding top_k for better response quality
-			},
-		},
-		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-		build = "make",
-		-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
-			"stevearc/dressing.nvim",
-			"nvim-lua/plenary.nvim",
-			"MunifTanjim/nui.nvim",
-			--- The below dependencies are optional,
-			"echasnovski/mini.pick", -- for file_selector provider mini.pick
-			"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-			"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-			"ibhagwan/fzf-lua", -- for file_selector provider fzf
-			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-			"zbirenbaum/copilot.lua", -- for providers='copilot'
-			{
-				-- support for image pasting
-				"HakonHarnes/img-clip.nvim",
-				event = "VeryLazy",
-				opts = {
-					-- recommended settings
-					default = {
-						embed_image_as_base64 = false,
-						prompt_for_file_name = false,
-						drag_and_drop = {
-							insert_mode = true,
-						},
-						-- required for Windows users
-						use_absolute_path = true,
-					},
-				},
-			},
-			{
-				-- Make sure to set this up properly if you have lazy=true
-				"MeanderingProgrammer/render-markdown.nvim",
-				opts = {
-					file_types = { "markdown", "Avante" },
-				},
-				ft = { "markdown", "Avante" },
-			},
-		},
-	},
+	-- 	{
+	-- 		"yetone/avante.nvim",
+	-- 		event = "VeryLazy",
+	-- 		version = false, -- Never set this value to "*"! Never!
+	-- 		keys = {
+	-- 			{ "<leader>ad", "<cmd>AvanteClear<cr>", desc = "Clear Avante context" },
+	-- 		},
+	-- 		opts = {
+	-- 			provider = "claude",
+	-- 			claude = {
+	-- 				model = "claude-3-7-sonnet-20240307",
+	-- 				timeout = 60000,
+	-- 				temperature = 0.1,
+	-- 				max_tokens = 12000,
+	-- 			},
+	-- 			web_search_engine = {
+	-- 				provider = "google",
+	-- 			},
+	-- 			rag_service = {
+	-- 				enabled = true,
+	-- 				host_mount = "~/fintoc",
+	-- 				provider = "openai",
+	-- 				endpoint = "https://api.openai.com/v1",
+	-- 				llm_model = "",
+	-- 				embed_model = "",
+	-- 			},
+	-- 		},
+	-- 		build = "make",
+	-- 		dependencies = {
+	-- 			"nvim-treesitter/nvim-treesitter",
+	-- 			"stevearc/dressing.nvim",
+	-- 			"nvim-lua/plenary.nvim",
+	-- 			"MunifTanjim/nui.nvim",
+	-- 			"echasnovski/mini.pick",
+	-- 			"nvim-telescope/telescope.nvim",
+	-- 			"hrsh7th/nvim-cmp",
+	-- 			"ibhagwan/fzf-lua",
+	-- 			"nvim-tree/nvim-web-devicons",
+	-- 			"zbirenbaum/copilot.lua",
+	-- 			{
+	-- 				"HakonHarnes/img-clip.nvim",
+	-- 				event = "VeryLazy",
+	-- 				opts = {
+	-- 					default = {
+	-- 						embed_image_as_base64 = false,
+	-- 						prompt_for_file_name = false,
+	-- 						drag_and_drop = {
+	-- 							insert_mode = true,
+	-- 						},
+	-- 						use_absolute_path = true,
+	-- 					},
+	-- 				},
+	-- 			},
+	-- 			{
+	-- 				"MeanderingProgrammer/render-markdown.nvim",
+	-- 				opts = {
+	-- 					file_types = { "markdown", "Avante" },
+	-- 				},
+	-- 				ft = { "markdown", "Avante" },
+	-- 			},
+	-- 		},
+	-- 	},
 }
