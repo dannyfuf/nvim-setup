cb34b6c0d907d3bc8d751b856b61f2a11f2ba852
Danny Fuentes <dannyfuf@gmail.com>
2025-06-22 15:54:36 -0400

    cleanup

--- lua/plugins/go_to_pr.lua
+++
@@ -1,3 +0,0 @@
-return {
-	{ "tvergara/go-to-pr.vim" },
-}
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -5,43 +5,6 @@ return {
 			"williamboman/mason-lspconfig.nvim",
 			"WhoIsSethDaniel/mason-tool-installer.nvim",
 		},
-		config = function()
-			local mason = require("mason")
-			local mason_lspconfig = require("mason-lspconfig")
-			local mason_tool_installer = require("mason-tool-installer")
-
-			mason.setup({
-				ui = {
-					icons = {
-						package_installed = "✓",
-						package_pending = "➜",
-						package_uninstalled = "✗",
-					},
-				},
-			})
-
-			mason_lspconfig.setup({
-				ensure_installed = {
-					"lua_ls",
-					"sorbet",
-					-- "ruby_lsp",
-					"ts_ls",
-					"eslint",
-					"gopls",
-					"denols",
-					"astro",
-				},
-				automatic_installation = true,
-				automatic_enable = false,
-			})
-
-			mason_tool_installer.setup({
-				ensure_installed = {
-					"stylua",
-					"eslint_d",
-					"rubocop",
-				},
-			})
-		end,
+		config = function() end,
 	},
 }
