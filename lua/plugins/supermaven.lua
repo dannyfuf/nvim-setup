50b8ab76db97f6ccc651b143407490dbed7f6487
Danny Fuentes <31667986+dannyfuf@users.noreply.github.com>
2026-06-11 15:56:30 -0400

    disable supermaven

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -1,14 +1,14 @@
 return {
-	{
-		"supermaven-inc/supermaven-nvim",
-		config = function()
-			require("supermaven-nvim").setup({
-				keymaps = {
-					accept_suggestion = "<Tab>",
-					clear_suggestion = "<S-Tab>",
-					accept_word = "<C-l>", -- Changed from <S-l> to <C-l> (Ctrl+L)
-				},
-			})
-		end,
-	},
+--	{
+--		"supermaven-inc/supermaven-nvim",
+--		config = function()
+--			require("supermaven-nvim").setup({
+--				keymaps = {
+--					accept_suggestion = "<Tab>",
+--					clear_suggestion = "<S-Tab>",
+--					accept_word = "<C-l>", -- Changed from <S-l> to <C-l> (Ctrl+L)
+--				},
+--			})
+--		end,
+--	},
 }
