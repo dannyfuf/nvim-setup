98defa3552800b3d4e3ddc6c25c64122693a532f
Danny Fuentes <dannyfuf@gmail.com>
2026-09-04 14:22:21 -0400

    feat: replace project search workflow

---
+++ lua/plugins/search.lua
@@ -0,0 +1,49 @@
+return {
+  {
+    "dmtrKovalenko/fff.nvim",
+    build = function()
+      require("fff.download").download_or_build_binary()
+    end,
+    lazy = false,
+    opts = {
+      debug = {
+        enabled = true,
+        show_scores = true,
+      },
+    },
+    keys = {
+      {
+        "<leader>sf",
+        function()
+          require("fff").find_files()
+        end,
+        desc = "FFFind files",
+      },
+      {
+        "<leader>sg",
+        function()
+          require("fff").live_grep()
+        end,
+        desc = "LiFFFe grep",
+      },
+      {
+        "<leader>sz",
+        function()
+          require("fff").live_grep({
+            grep = {
+              modes = { "fuzzy", "plain" },
+            },
+          })
+        end,
+        desc = "Live fffuzy grep",
+      },
+      {
+        "<leader>sc",
+        function()
+          require("fff").live_grep({ query = vim.fn.expand("<cword>") })
+        end,
+        desc = "Search current word",
+      },
+    },
+  },
+}
