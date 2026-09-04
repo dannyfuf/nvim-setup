91b6c34b7143b4e50a44ae16b87114939fc80fde
Danny Fuentes <dannyfuf@gmail.com>
2026-09-04 14:22:20 -0400

    feat: expand formatter coverage

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -4,7 +4,13 @@ return {
     config = function()
       require("conform").setup({
         formatters_by_ft = {
+          javascript = { "biome" },
+          javascriptreact = { "biome" },
           python = { "black" },
+          typescript = { "biome" },
+          typescriptreact = { "biome" },
+          json = { "biome" },
+          jsonc = { "biome" },
         },
         format_on_save = {
           timeout_ms = 100000,
